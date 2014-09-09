fcabb84ece417367815b08bf98d7e3ed0e62607b
unknown <vr222bt@student.lnu.se>
2014-09-10 00:05:26 +0200

    test

---
+++ vaxelpengar/vaxelpengar/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vaxelpengar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Deklarera variabler
+
+            double summa;
+            int betalat;
+
+            //Läs in värden
+
+            Console.Write("Ange totalsumman:");
+            summa = double.Parse(Console.ReadLine());
+            
+
+
+            //Räkna ut differensen och skriv ut pengar att ge tillbaka
+
+            //Räkna ut antalet av 500kr 100kr 20kr 10kr 5kr 1kr som ska ges tillbaka
+
+            //Skriv ut antalet av olika sedlar/mynt
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
