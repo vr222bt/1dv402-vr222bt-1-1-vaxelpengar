f531e66d35ce3c2eca3decc95c0b632db8468b14
unknown <vr222bt@student.lnu.se>
2014-09-10 10:28:47 +0200

    Arbetade med växelpengar,inmatning samt avrunding

--- vaxelpengar/vaxelpengar/Program.cs
+++ vaxelpengar/vaxelpengar/Program.cs
@@ -14,15 +14,29 @@ namespace vaxelpengar
 
             double summa;
             int betalat;
-
+            int attbetala;
+            int växel;
             //Läs in värden
 
             Console.Write("Ange totalsumman:");
             summa = double.Parse(Console.ReadLine());
+
+            Console.Write("Ange erhållet belopp:");
+            betalat = int.Parse(Console.ReadLine());
             
 
 
             //Räkna ut differensen och skriv ut pengar att ge tillbaka
+
+            attbetala = Convert.ToInt32(Math.Round(summa));
+
+            Console.WriteLine("Att betala: {0}", attbetala);
+
+            växel = betalat - attbetala;
+
+            Console.WriteLine("Pengar tillbaka: {0}", växel);
+
+            //Skriv ut växel samt 
 
             //Räkna ut antalet av 500kr 100kr 20kr 10kr 5kr 1kr som ska ges tillbaka
 
